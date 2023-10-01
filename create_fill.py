830fa8aaade82143222e8ebf0ec412516fb2796b
ルスラン ブドビキン <banievro@gmail.com>
2023-10-01 17:16:04 +0900

    Version 2.1 (Changes): Support for Images with Alpha Channel: The program can now process images both with an alpha channel (transparent areas) and without it.
    Brightness Increase by 40%: Image brightness is now increased by 40%, making the images brighter.
    
    Utilization of Multithreading: To improve image processing performance, the program now employs multithreading. Images with alpha channels and without are processed concurrently in separate threads.
    
    Minor Bug Fixes: During the update, minor bugs were fixed, and the code structure was enhanced for improved readability and reliability.

--- create_fill.py
+++ create_fill.py
@@ -1,48 +1,71 @@
 from PIL import Image, ImageEnhance
 import os
+import multiprocessing
 
-# Directory paths for input and saved images
-input_folder = r"C:\Night_Fill\Image"
-output_folder = r"C:\Night_Fill\Ready"
 
-# Check for the presence of the output_folder folder and create it if it does not exist
-if not os.path.exists(input_folder):
-    os.makedirs(input_folder)
-
-if not os.path.exists(output_folder):
-    os.makedirs(output_folder)
-
-# Get a list of files in the input directory
-input_files = os.listdir(input_folder)
-
-# Iterate through all files in the directory
-for filename in input_files:
-    # Create full paths to the input and output files
-    input_image_path = os.path.join(input_folder, filename)
-    output_image_path = os.path.join(output_folder, f"{os.path.splitext(filename)[0]}_fill.png")
-
+def process_image(input_image_path, output_image_path):
     # Open the image using Pillow
     image = Image.open(input_image_path)
 
-    # Increase brightness by 50%
+    # Increase brightness by 40%
     enhancer = ImageEnhance.Brightness(image)
-    image = enhancer.enhance(0.75)  # Increase by 50%
+    image = enhancer.enhance(1.1)  # Increase by 40%
 
-    # Adjust brightness by -65%
-    enhancer = ImageEnhance.Brightness(image)
-    image = enhancer.enhance(1 - 0.01 * 5)
+    # Check for the presence of an alpha channel
+    if image.mode == 'RGBA':
+        r, g, b, a = image.split()
+        r = r.point(lambda i: i - 255)
+        g = g.point(lambda i: i - 163)
+        image = Image.merge('RGBA', (r, g, b, a))
+    elif image.mode == 'RGB':
+        r, g, b = image.split()
+        r = r.point(lambda i: i - 255)
+        g = g.point(lambda i: i - 163)
+        image = Image.merge('RGB', (r, g, b))
+    else:
+        # If the image has a different mode (e.g., grayscale), save it without modifications
+        pass
 
-    # Modify the red channel by -255
-    r, g, b = image.split()
-    r = r.point(lambda i: i - 255)
-
-    # Modify the green channel by -163
-    g = g.point(lambda i: i - 163)
-
-    # Merge the channels back into an image
-    image = Image.merge('RGB', (r, g, b))
-
-    # Save the darkened image
+    # Save the processed image
     image.save(output_image_path)
 
-    print(f"画像 {filename} が正常に暗くなり、{os.path.basename(output_image_path)} として保存されました。")
+    print(f"{input_image_path} 画像が処理され、{output_image_path} として保存されました。")
+
+
+def main():
+    # Directory paths for input and saved images
+    input_folder = r"C:\Night_Fill\Image"
+    output_folder = r"C:\Night_Fill\Ready"
+
+    # Check for the presence of the output_folder folder and create it if it does not exist
+    if not os.path.exists(input_folder):
+        os.makedirs(input_folder)
+
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    # Get a list of files in the input directory
+    input_files = os.listdir(input_folder)
+
+    # Separate images into two groups: with alpha channels and without
+    images_with_alpha = []
+    images_without_alpha = []
+
+    for filename in input_files:
+        input_image_path = os.path.join(input_folder, filename)
+        output_image_path = os.path.join(output_folder, f"{os.path.splitext(filename)[0]}_fill.png")
+
+        image = Image.open(input_image_path)
+        if 'A' in image.getbands():
+            images_with_alpha.append((input_image_path, output_image_path))
+        else:
+            images_without_alpha.append((input_image_path, output_image_path))
+
+    # Create processes for processing images with alpha channels
+    with multiprocessing.Pool() as pool:
+        pool.starmap(process_image, images_with_alpha)
+        pool.starmap(process_image, images_without_alpha)
+
+
+if __name__ == "__main__":
+    main()
